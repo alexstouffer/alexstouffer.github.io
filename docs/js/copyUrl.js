6ba3d31792f6e09e1d55679374fcdc7246ca825d
alexstouffer <bizkite@hotmail.com>
2021-05-05 16:22:05 -0700

    rebuild, resubmit

--- docs/js/copyUrl.js
+++ docs/js/copyUrl.js
@@ -1,18 +1,18 @@
-document.getElementById("copyButton").addEventListener("click", copyToClipboard);
+// document.getElementById("copyButton").addEventListener("click", copyToClipboard);
     
-let currentUrl = window.location;
-let copyTextContainer = document.getElementById("copyText");
-let toolTip = document.getElementById("toolTip");
+// let currentUrl = window.location;
+// let copyTextContainer = document.getElementById("copyText");
+// let toolTip = document.getElementById("toolTip");
 
-function copyToClipboard(){
-    copyTextContainer.value = currentUrl
-    copyTextContainer.focus();
-    copyTextContainer.select();
-    document.execCommand("copy");
+// function copyToClipboard(){
+//     copyTextContainer.value = currentUrl
+//     copyTextContainer.focus();
+//     copyTextContainer.select();
+//     document.execCommand("copy");
 
-    toolTip.style.animationName = "pan-toolbar";
+//     toolTip.style.animationName = "pan-toolbar";
 
-    setTimeout(function(){
-         toolTip.style.removeProperty('animation-name');
-    }, 2000);
-}
+//     setTimeout(function(){
+//          toolTip.style.removeProperty('animation-name');
+//     }, 2000);
+// }
