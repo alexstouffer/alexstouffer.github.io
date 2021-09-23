292c47b6f3b9b102af6acadd70a4d7a3fc9bd7c4
alexstouffer <bizkite@hotmail.com>
2021-09-23 14:19:07 -0700

    update theme colors

--- themes/hugo-theme-novela-master/assets/js/toggleColors.js
+++ themes/hugo-theme-novela-master/assets/js/toggleColors.js
@@ -67,10 +67,10 @@ function makeDark(){
     bodyElement.style.setProperty('--primary', "#fff");
     bodyElement.style.setProperty('--secondary', "#fff");
     bodyElement.style.setProperty('--grey', "#73737D");
-    bodyElement.style.setProperty('--background-color', "#111216");
+    bodyElement.style.setProperty('--background-color', "#151b31");
     bodyElement.style.setProperty('--accent', "#E9DAAC");
     bodyElement.style.setProperty('--hover', "rgba(255, 255, 255, 0.07)");
-    bodyElement.style.setProperty('--gradient', "linear-gradient(180deg, #111216 0%, rgba(66, 81, 98, 0.36) 100%)");
+    bodyElement.style.setProperty('--gradient', "linear-gradient(180deg, #151b31 0%, rgba(66, 81, 98, 0.36) 100%)");
     bodyElement.style.setProperty('--articleText', "#fff");
     bodyElement.style.setProperty('--track', "rgba(255, 255, 255, 0.3)");
     bodyElement.style.setProperty('--progress', "#fff");
